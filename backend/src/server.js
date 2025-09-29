4dc0474986bcfcb93d08342d5f70c5ab75353170
giangkh1908 <giangkh1908@gmail.com>
2025-09-29 16:50:51 +0700

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,33 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import ragRoutes from "./routes/ragRoutes.js";
 import { testConnection } from "./utils/db.js";
+import path from "path";
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 
 // Routes
 app.use("/api/rag", ragRoutes);
 
 // Start server
-app.listen(5000, async () => {
-  console.log('🚀 Server is running on port 5000');
-  console.log('🔄 Testing database connection...');
+app.listen(PORT, async () => {
+  console.log("🚀 Server is running on port 5000");
+  console.log("🔄 Testing database connection...");
   await testConnection();
 });
--- frontend/src/components/ChatAi.jsx
+++ frontend/src/components/ChatAi.jsx
@@ -16,7 +16,9 @@ const ChatAi = () => {
   const textareaRef = useRef(null);
 
   // API base URL - đổi theo port backend của bạn
-  const API_BASE_URL = 'http://localhost:5000';
+  const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/api";
+  
+   const API_BASE_URL = BASE_URL;
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
